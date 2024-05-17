15768ab50976528a0c9b1a4fddec3e8b9ca73bcd
Mr-Rovilay <ayodejiakinola007@gmail.com>
2024-05-17 14:36:29 +0100

    package.json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import DeleteBook from "./paages/DeleteBook";
 const App = () => {
   return (
     <Routes>
-      <Route path="" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/books/create" element={<CreateBook />} />
       <Route path="/books/details/:id" element={<ShowBook />} />
       <Route path="/books/edit/:id" element={<EditBook />} />
